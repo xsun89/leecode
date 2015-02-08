0159a7a3c9d9d4d7d0aed33b83da97620daab36a
xsun89 <sunxin89@gmail.com>
2015-02-07 16:52:57 -0800

    add

---
+++ CheckTheCheck/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+char status[8][8];
+
+int main (int ac, char *av[])
+{
+    memset(status, -1, sizeof(status));
+    string line;
+
+    while(getline(cin, line))
+    {
+
+
+    }
+    return 0;
+}
